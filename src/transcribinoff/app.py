6c5d1ee51aba8d6d70dd9452c2339e0bac13ebc7
reubinoff <moshe@primesec.ai>
2024-10-22 10:17:37 +0300

    test

---
+++ src/transcribinoff/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, APIRouter
+from transcribinoff.routers import users_router
+
+routers = [users_router]
+root_router = APIRouter()
+for router in routers:
+    root_router.include_router(router)
+
+app = FastAPI(
+    title="TranscribinOff",
+    description="TranscribinOff is a web application that allows users to transcribe audio files.",
+    version="0.1.0",
+)
+app.include_router(root_router)
+
